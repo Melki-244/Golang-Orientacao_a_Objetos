625751c0904461668850c961c677347bd7fb107e
Melki-244 <melkicavalcante244@gmail.com>
2022-05-14 14:05:29 -0300

    Referência Ponteiros e Métodos | Criando O Método Sacar

--- Banco/main.go
+++ Banco/main.go
@@ -4,39 +4,30 @@ import "fmt"
 
 /* Criando Uma Estrutura em Go*/
 type ContaCorrente struct {
-  nome string
+  titular string
   numeroDaConta int
   numeroDaAgencia int
   saldo float64
 }
+func (conta *ContaCorrente) Sacar(valorDoSaque float64) string  {
+  podeSacar := valorDoSaque >= 0 && valorDoSaque <= conta.saldo 
+
+  if podeSacar {
+    conta.saldo -= valorDoSaque 
+    return "Saque Realizado Com Sucesso"
+  }else {
+    return "Saldo Insulficiente"
+  }
+}
 
 func main()  {
+  contaDaSilvia := ContaCorrente{}
+  contaDaSilvia.titular = "Silvia" 
+  contaDaSilvia.saldo = 77.94
 
-  /*Atribuindo valores de uma estrutura forma Explicita*/
-  contaGuilherme := ContaCorrente{nome: "Guilherme", numeroDaConta: 111333, numeroDaAgencia:999000, saldo: 30.58} 
-  contaGuilherme2 := ContaCorrente{nome: "Guilherme", numeroDaConta: 111333, numeroDaAgencia:999000, saldo: 30.58} 
-  /* Atribuindo Valores de uma estrutura de forma Impicita*/
-  contaMelki := ContaCorrente{"Melki", 111222, 999111, 99.08}
-  contaMelki2 := ContaCorrente{"Melki", 111222, 999111, 99.08}
-
-  fmt.Println(contaMelki == contaMelki2)
-  fmt.Println(contaGuilherme == contaGuilherme2)
-
-  /* Neste Exemplo vemos um modo diferente de como trabalhar com dados de uma estrutura 
-  ultilizando o metodo new
-  */
-  var contaDaChris *ContaCorrente  
-  contaDaChris = new(ContaCorrente) 
-  contaDaChris.nome = "Chris"
-  contaDaChris.saldo = 75.88
+  fmt.Println(contaDaSilvia.saldo)
   
-  var contaDaChris2 *ContaCorrente  
-  contaDaChris = new(ContaCorrente) 
-  contaDaChris.nome = "Chris"
-  contaDaChris.saldo = 75.88
+  fmt.Println(contaDaSilvia.Sacar(-50))
 
-  fmt.Println(&contaDaChris)
-  fmt.Println(&contaDaChris2)
-
-  fmt.Println(*contaDaChris == *contaDaChris2)
+  fmt.Println(contaDaSilvia.saldo)
 }
