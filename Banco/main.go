c82469a6291baaa3664eba8c92e301080b692ba5
Melki-244 <melkicavalcante244@gmail.com>
2022-05-14 11:13:58 -0300

    Referência Ponteiros e Métodos | New e Ponteiros

--- Banco/main.go
+++ Banco/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+/* Criando Uma Estrutura em Go*/
 type ContaCorrente struct {
   nome string
   numeroDaConta int
@@ -10,9 +11,22 @@ type ContaCorrente struct {
 }
 
 func main()  {
+
+  /*Atribuindo valores de forma Explicita*/
   contaGuilherme := ContaCorrente{nome: "Guilherme", numeroDaConta: 111333, numeroDaAgencia:999000, saldo: 30.58} 
+  /* Atribuindo Valores de forma Impicita*/
+  contaMelki := ContaCorrente{"Melki", 111222, 999111, 99.08}
+
+  fmt.Println(contaMelki)
   fmt.Println(contaGuilherme)
 
-  contaMelki := ContaCorrente{"Melki", 111222, 999111, 99.08}
-  fmt.Println(contaMelki)
+  /* Neste Exemplo vemos um modo diferente de como trabalhar com dados de uma estrutura 
+  ultilizando o metodo new
+  */
+  var contaDaChris *ContaCorrente  
+  contaDaChris = new(ContaCorrente) 
+  contaDaChris.nome = "Chris"
+  contaDaChris.saldo = 75.88
+
+  fmt.Println(contaDaChris)
 }
