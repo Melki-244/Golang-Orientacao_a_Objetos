8e95a22c5d2b388ffe58abb5a198f039180a267a
Melki-244 <melkicavalcante244@gmail.com>
2022-05-19 13:21:37 -0300

    Interface E Novo Tipo de Conta | Interface

--- Banco/main.go
+++ Banco/main.go
@@ -7,10 +7,17 @@ import (
 	 "github.com/Melki-244/Golang-Orientacao_a_Objetos/Banco/contas"
 )
 
+func pagaBoleto(conta verificaConta,valorDoBoleto float64) {
+  conta.Saca(valorDoBoleto) 
+}
+type verificaConta interface {
+  Saca(valor float64) (string,float64)  
+}
 func main()  {
   clienteKatia := clientes.Titular{"Katia", "000.000.000-00", "Sei Lá"}
   contaDaKatia := contas.ContaPoupanca{Titular: clienteKatia, NumeroDaAgencia: 222333, NumeroDaConta: 444000222}
 
   contaDaKatia.Deposita(200)
+  pagaBoleto(&contaDaKatia, 200)
   f.Println(contaDaKatia.ObterSaldo())
 }
